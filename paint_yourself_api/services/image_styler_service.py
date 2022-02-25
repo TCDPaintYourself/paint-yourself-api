8e2238d0d17ce94cbd08a3ecf4a288916a81d5ab
David Kubala <dkubala@Davids-MacBook-Pro-2.local>
2022-02-25 11:35:10 +0000

    Stylization Code

--- paint_yourself_api/services/image_styler_service.py
+++ paint_yourself_api/services/image_styler_service.py
@@ -2,19 +2,39 @@ import io
 import typing
 
 from paint_yourself_api.schemas import StyledImageThemeEnum
-
+from paint_yourself_api.vgg_style_transfer import StyleTransfer
+import cv2 as cv
+import numpy as np
 
 class ImageStylerService:
     """Service used to style user submitted images."""
 
     def create_styled_image(
         self, image: typing.BinaryIO, reference_image: typing.BinaryIO
-    ) -> typing.BinaryIO:
+) -> typing.BinaryIO:
+
         """Applies the reference image style to the image."""
 
         # TODO: Apply style to the image.
         with image as f:
-            return io.BytesIO(f.read())
+            with reference_image as r_f:
+                f_bytes = f.read()
+                r_f_bytes = r_f.read()
+
+                input_image = np.array(f_bytes, dtype=np.uint8)
+                theme_image = np.array(r_f_bytes, dtype=np.uint8)
+                input_path = './paint-yourself-api/paint_yourself_api/input/input.png'
+                theme_path = './paint-yourself-api/paint_yourself_api/input/theme.png'
+                cv.imwrite(input_image, input_path)
+                cv.imwrite(theme_image, theme_path)
+
+                styler = StyleTransfer()
+                stylized = styler.paint_image('./samples/Daniel.png', './styles/Cezanne/215466.jpg')
+
+                is_success, im_buf_arr = cv.imencode(".jpg", stylized)
+                byte_im = im_buf_arr.tobytes()
+
+                return byte_im
 
     def create_themed_image(
         self, image: typing.BinaryIO, theme: StyledImageThemeEnum
@@ -23,4 +43,11 @@ class ImageStylerService:
 
         # TODO: Apply theme to the image.
         with image as f:
+
+
+
+
+
+
+
             return io.BytesIO(f.read())
---
+++ paint_yourself_api/services/vgg_style_transfer.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import cv2 as cv
+import numpy as np
+import PIL.Image
+import os
+import tensorflow_hub as hub
+
+class StyleTransfer:
+    def __init__(self):
+        os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+        self.model = hub.load('../image_stylization_model/')
+
+    def tensor_to_image(self, tensor):
+      tensor = tensor * 255
+      tensor = np.array(tensor, dtype=np.uint8)
+      if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+      return PIL.Image.fromarray(tensor)
+
+    def load_img(self, path_to_img):
+      max_dim = 1024
+      img = tf.io.read_file(path_to_img)
+      img = tf.image.decode_image(img, channels=3)
+      img = tf.image.convert_image_dtype(img, tf.float32)
+
+      shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+      long_dim = max(shape)
+      scale = max_dim / long_dim
+
+      new_shape = tf.cast(shape * scale, tf.int32)
+
+      img = tf.image.resize(img, new_shape)
+      img = img[tf.newaxis, :]
+      return img
+
+    def paint_image(self, input_image_path, input_style_path):
+        # Load an input image & style into a tensor
+        input_image = self.load_img(input_image_path)
+        input_style = self.load_img(input_style_path)
+
+        # # Show the image and reference image
+        # cv.imshow('image', cv.cvtColor(np.array(self.tensor_to_image(input_image)), cv.COLOR_BGR2RGB))
+        # cv.imshow('reference', cv.cvtColor(np.array(self.tensor_to_image(input_style)), cv.COLOR_BGR2RGB))
+
+        # Load the co
+        stylized_image = self.model(tf.constant(input_image), tf.constant(input_style))[0]
+        stylized_image = np.array(self.tensor_to_image(stylized_image))
+
+        # Adjust contrast
+        hsvImg = cv.cvtColor(stylized_image, cv.COLOR_BGR2HSV)
+        hsvImg[..., 1] = (hsvImg[..., 1] * 1.5).clip(0, 255)
+        hsvImg[..., 2] = (hsvImg[..., 2] * 1.1).clip(0, 255)
+        stylized_image = cv.cvtColor(hsvImg, cv.COLOR_HSV2BGR)
+
+        # cv.imshow('stylized_img', cv.cvtColor(stylized_image, cv.COLOR_BGR2RGB))
+        # cv.waitKey(0)
+
+        return stylized_image
+
+if __name__ == '__main__':
+    # Load the painting model
+    s = StyleTransfer()
+
+    image = s.paint_image('./samples/Daniel.png', './styles/Cezanne/215466.jpg')
+
+    cv.imshow('image', image)
+    cv.waitKey(0)
