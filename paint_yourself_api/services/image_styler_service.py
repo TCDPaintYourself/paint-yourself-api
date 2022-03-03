3c372b3095677988a60275e9aef5ad9bf157f831
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-03-03 18:20:16 +0000

    Change naming convention

--- paint_yourself_api/services/image_styler_service.py
+++ paint_yourself_api/services/image_styler_service.py
@@ -19,34 +19,34 @@ class ImageStylerService:
         self.styler = styler
 
     def create_styled_image(
-        self, image: typing.BinaryIO, reference_image: typing.BinaryIO
+        self, input_image: typing.BinaryIO, reference_image: typing.BinaryIO
     ) -> typing.BinaryIO:
         """Applies the reference image style to the image."""
 
-        with image as f:
-            with reference_image as r_f:
-                f_bytes = f.read()
-                r_bytes = r_f.read()
+        with input_image as image_file:
+            with reference_image as reference_image_file:
+                image_bytes = image_file.read()
+                reference_image_bytes = reference_image_file.read()
 
-                stylized = self.styler.paint_image(f_bytes, r_bytes)
+                stylized = self.styler.paint_image(image_bytes, reference_image_bytes)
 
-                is_success, im_buf_arr = cv.imencode(".jpg", stylized)
+                _, im_buf_arr = cv.imencode(".jpg", stylized)
                 byte_im = im_buf_arr.tobytes()
 
                 return io.BytesIO(byte_im)
 
     def create_themed_image(
-        self, image: typing.BinaryIO, theme: StyledImageThemeEnum
+        self, input_image: typing.BinaryIO, theme: StyledImageThemeEnum
     ) -> typing.BinaryIO:
         """Applies a theme to the image."""
 
         theme_image_path = f"./paint_yourself_api/themes/{theme.value}.jpg"
 
-        with image as f:
-            with open(theme_image_path, "rb") as t_f:
-                f_bytes = f.read()
-                t_bytes = t_f.read()
-                stylized = self.styler.paint_image(f_bytes, t_bytes)
+        with input_image as input_image_file:
+            with open(theme_image_path, "rb") as theme_image_file:
+                image_bytes = input_image_file.read()
+                theme_bytes = theme_image_file.read()
+                stylized = self.styler.paint_image(image_bytes, theme_bytes)
 
                 _, im_buf_arr = cv.imencode(".jpg", stylized)
                 byte_im = im_buf_arr.tobytes()
