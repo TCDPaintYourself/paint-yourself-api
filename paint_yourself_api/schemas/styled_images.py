c4510d2e699ba192e9174844cca4833719760fec
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-03-03 16:01:33 +0000

    Change theme enum values

--- paint_yourself_api/schemas/styled_images.py
+++ paint_yourself_api/schemas/styled_images.py
@@ -4,10 +4,10 @@ from enum import Enum
 class StyledImageThemeEnum(str, Enum):
     """Enum containing the allowed themes."""
 
-    van_gogh = "Van Gogh"
-    claude_monet = "Claude Monet"
-    rembrandt = "Rembrandt"
-    whistler = "Whistler"
-    picasso = "Picasso"
-    da_vinci = "Da Vinci"
-    caravaggio = "Caravaggio"
+    van_gogh = "van-gogh"
+    claude_monet = "claude-monet"
+    rembrandt = "rembrandt"
+    whistler = "whistler"
+    picasso = "picasso"
+    da_vinci = "da-vinci"
+    caravaggio = "caravaggio"
